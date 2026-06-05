e9a82edd637269d7dfb0435a94e1b720c0c807c4
consuelita <adelita@operatorfoundation.org>
2026-06-04 21:11:08 -0500

    Added sampleRate to ToneMode

--- andFlmsg/src/main/java/com/AndFlmsg/ToneMode.java
+++ andFlmsg/src/main/java/com/AndFlmsg/ToneMode.java
@@ -14,22 +14,33 @@ package com.AndFlmsg;
  * getModeCodeByName. The toneModesResolveToValidCodes test fails if they disagree.
  */
 public enum ToneMode {
-    MFSK8("MFSK8"),
-    MFSK16("MFSK16"),
-    MFSK32("MFSK32"),
-    MFSK64("MFSK64"),
-    MFSK128("MFSK128"),
-    MFSK64L("MFSK64L"),
-    MFSK128L("MFSK128L");
+    MFSK8("MFSK8", 8000),
+    MFSK16("MFSK16", 8000),
+    MFSK32("MFSK32", 8000),
+    MFSK64("MFSK64", 8000),
+    MFSK128("MFSK128", 8000),
+    MFSK64L("MFSK64L", 8000),
+    MFSK128L("MFSK128L", 8000);
 
     private final String fldigiName;
+    private final int sampleRate;
 
-    ToneMode(String fldigiName) {
+    ToneMode(String fldigiName, int sampleRate) {
+
         this.fldigiName = fldigiName;
+        this.sampleRate = sampleRate;
     }
 
     /** Native globals.h enum code for this mode, or -1 if unsupported. */
     public int code() {
         return Modem.getModeCodeByName(fldigiName);
+    }
+
+    /**
+     * Sample rate in Hz used by the fldigi modem for this mode.
+     * Sourced from MFSKSampleRate in mfsk.h.
+     */
+    public int sampleRate() {
+        return sampleRate;
     }
 }
